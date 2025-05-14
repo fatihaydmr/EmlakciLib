16e6f49420daa0d60dc4fe679f8267ee9336e6b9
fatihaydmr <fatihaydemir078@gmail.com>
2025-05-14 13:14:21 +0300

    Add files via upload

---
+++ KiralikEv.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Emlakci.Lib
+{
+    public class KiralikEv : Ev
+    {
+
+        public KiralikEv() 
+        {
+        }
+
+        public KiralikEv(int odasayisi, int katno, double alan, double kira, double depozito, string semt = "Kızılay") //opsiyonel parametre kullanımı(değer girmezse otomatik kızılay atanır)
+        {
+            base.Odasayisi = odasayisi;
+            base.Katno = katno;
+            base.Alan = alan;
+            base.Semt = semt;
+            this.Kira = kira;
+            this.Depozito = depozito;
+            
+        }
+
+        public double Kira { get; set; }
+
+        public double Depozito { get; set; }
+
+    }
+}
+
+/*
+internal: Yalnızca aynı namespace içindeki classlar tarafından erişilebilir.(access modifiers)
+her class ın yalnızca bir tane base classı olabilir
+
+*/
---
+++ SatilikEv.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Emlakci.Lib
+{
+    public class SatilikEv : Ev
+    {
+        public SatilikEv()
+        {
+        }
+
+
+
+        public SatilikEv(double satisfiyat, int odasayisi, int katno, double alan, string semt = "Kızılay") : base(odasayisi, katno, alan, semt)
+        {
+            Satisfiyat = satisfiyat;
+        }
+        // içeriye tek tek base.odasayisi vs yazmamıza gerek kalmadı
+
+        public double Satisfiyat { get; set; }
+
+
+    }
+}
